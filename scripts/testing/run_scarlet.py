123de275b6fd887ae55d15997b455805a4dbb055
b-biswas <bbiswas.dgp@gmail.com>
2024-04-23 12:00:57 +0200

    update scarlet configuration

--- scripts/testing/run_scarlet.py
+++ scripts/testing/run_scarlet.py
@@ -16,8 +16,12 @@ import scarlet
 import scarlet.psf
 import sep
 import yaml
-from madness_deblender.metrics import compute_aperture_photometry, compute_pixel_cosdist
 
+from madness_benchmark.metrics import (
+    compute_aperture_photometry,
+    compute_pixel_cosdist,
+    compute_shapes,
+)
 from madness_benchmark.utils import get_benchmark_config_path
 
 # logging level set to INFO
@@ -82,32 +86,35 @@ def predict_with_scarlet(image, x_pos, y_pos, show_scene, show_sources, filters)
         sig.append(sep.Background(image[i]).globalrms)
         weights[i] = weights[i] / (sig[i] ** 2)
     observation = scarlet.Observation(
-        image, psf=scarlet.psf.ImagePSF(psf), weights=weights, channels=bands, wcs=wcs
+        image, psf=scarlet.psf.ImagePSF(psf), weights=weights, channels=bands
     )
 
     model_psf = scarlet.GaussianPSF(
         sigma=np.asarray(psf_fwhm) / 2.355
     )  # These numbers are derived from the FWHM given for LSST filters in the galcheat v1.0 repo https://github.com/aboucaud/galcheat/blob/main/galcheat/data/LSST.yaml
-    model_frame = scarlet.Frame(image.shape, psf=model_psf, channels=filters, wcs=wcs)
+    model_frame = scarlet.Frame(image.shape, psf=model_psf, channels=filters)
 
     observation = observation.match(model_frame)
     sources = []
-    for i in range(len(x_pos)):
-        result = scarlet.ExtendedSource(
-            model_frame,
-            model_frame.get_sky_coord((x_pos[i], y_pos[i])),
-            observation,
-            thresh=1,
-            shifting=True,
-        )
-        sources.append(result)
+    centers = [(x_pos[i], y_pos[i]) for i in range(len(x_pos))]
 
-    scarlet.initialization.set_spectra_to_match(sources, observation)
+    sources, skipped = scarlet.initialization.init_all_sources(
+                                                            model_frame,
+                                                            centers,
+                                                            observation,
+                                                            max_components=2,
+                                                            min_snr=50,
+                                                            thresh=1,
+                                                            shifting=True,
+                                                            fallback=True,
+                                                            silent=True,
+                                                            set_spectra=True,
+                                                        )
 
     scarlet_blend = scarlet.Blend(sources, observation)
 
     t0 = time.time()
-    scarlet_blend.fit(200, e_rel=1e-5)
+    scarlet_blend.fit(200, e_rel=1e-6)
     t1 = time.time()
 
     LOG.info("SCARLET TIME: " + str(t1 - t0))
@@ -136,6 +143,9 @@ def predict_with_scarlet(image, x_pos, y_pos, show_scene, show_sources, filters)
             add_boxes=True,
         )
         plt.show()
+
+    # scarlet.display.show_likelihood(scarlet_blend)
+    # plt.show()
 
     predicted_sources = []
     for src in sources:
@@ -166,7 +176,6 @@ for file_num in range(num_repetations):
         ]
     )
     bands = [f for f in survey._filters]
-    wcs = blend.wcs
 
     x_pos = blend.catalog_list[0]["y_peak"]
     y_pos = blend.catalog_list[0]["x_peak"]
@@ -264,6 +273,16 @@ for file_num in range(num_repetations):
             survey=survey,
         )
         scarlet_current_res.update(scarlet_photometry_current)
+
+        scarlet_shapes_current = compute_shapes(
+            field_image=blend.blend_images[field_num],
+            predictions=scarlet_current_predictions,
+            xpos=blend.catalog_list[field_num]["x_peak"],
+            ypos=blend.catalog_list[field_num]["y_peak"],
+            survey=survey,
+        )
+        scarlet_current_res.update(scarlet_shapes_current)
+
         scarlet_current_res = pd.DataFrame.from_dict(scarlet_current_res)
         scarlet_results.append(scarlet_current_res)
     # scarlet_results = vstack(scarlet_results)
